58fd1cccb7e2989b7ebaf9c625b1fade726ae283
Ravi Gupta <ravigupta7986@gmail.com>
2021-06-13 15:42:19 +0530

    avoid createRef to get value of textbox

--- src/main-page/addTodo.js
+++ src/main-page/addTodo.js
@@ -1,4 +1,6 @@
 import React from 'react';
+//TODO: Check if having useState statement here makes sense, despite having the same in separate hooks file 
+import { useState } from 'react';
 
 function getGuid() {
     return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
@@ -6,35 +8,39 @@ function getGuid() {
     );
 }
 
-//TODO: Check if there is a better way to handle textbox value than this
-var todoInputBox = React.createRef();
-
-var handleKeyDown = (e, props) => {
-    if (e.key === 'Enter') {
-        addNew(props);
-    }
-}
-
-var addNew = (props) => {
-    if (todoInputBox.current.value) {
-        props.addNewTodo({
-            "id": getGuid(),
-            "text": todoInputBox.current.value,
-            "status": "new"
-        });
-        todoInputBox.current.value = '';
-    } else {
-        todoInputBox.current.focus();
-    }
-}
-
 const AddNewTodo = (props) => {
+    const [todoInput, setTodoInput] = useState('');
+
+    var addNew = (props) => {
+        if (todoInput) {
+            props.addNewTodo({
+                "id": getGuid(),
+                "text": todoInput,
+                "status": "new"
+            });
+            setTodoInput('');
+        } else {
+            //todoInputBox.current.focus();
+            //TODO: find some mechanism for getting focus on textbox without use createRef
+        }
+    }
+
+    const handleChange = (event) => {
+      setTodoInput(event.target.value);
+    }
+
+    var handleKeyDown = (e, props) => {
+        if (e.key === 'Enter') {
+            addNew(props);
+        }
+    }
+
     return (
         <div className="row">
             <div className="col-2">Add Todo:</div>
             <div className="col-8">
-                <input ref={todoInputBox} type="text" id="toDoBox" className="addToDoBox"
-                    placeholder="Add todo..." onKeyDown={(e) => { handleKeyDown(e, props) }} />
+                <input type="text" id="toDoBox" className="addToDoBox" placeholder="Add todo..." value={todoInput} 
+                    onChange={handleChange} onKeyDown={(e) => { handleKeyDown(e, props) }} />
             </div>
             <div className="col-2">
                 <button type="button" onClick={() => { addNew(props); }}>Add</button>
@@ -44,51 +50,3 @@ const AddNewTodo = (props) => {
 }
 
 export default AddNewTodo;
-
-// class addNewTodo extends Component {
-//     state = {}
-
-//     todoInputBox = React.createRef();
-
-//     getGuid() {
-//         return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
-//             (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-//         );
-//     }
-
-//     addNew = (event) => {
-//         if (this.todoInputBox.current.value) {
-//             this.props.addNewTodo({
-//                 "id": this.getGuid(),
-//                 "text": this.todoInputBox.current.value,
-//                 "status": "new"
-//             });
-//             this.todoInputBox.current.value = '';
-//         } else {
-//             this.todoInputBox.current.focus();
-//         }
-//     }
-
-//     handleKeyDown = (e) => {
-//         if (e.key === 'Enter') {
-//             this.addNew(e);
-//         }
-//     }
-
-//     render() {
-//         return (
-//             <div className="row">
-//                 <div className="col-2">Add Todo:</div>
-//                 <div className="col-8">
-//                     <input ref={this.todoInputBox} type="text" id="toDoBox" className="addToDoBox" 
-//                             placeholder="Add todo..." onKeyDown={this.handleKeyDown} />
-//                 </div>
-//                 <div className="col-2">
-//                     <button type="button" onClick={this.addNew}>Add</button>
-//                 </div>
-//             </div>
-//         );
-//     }
-// }
-
-// export default addNewTodo;
